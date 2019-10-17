224a81db6cb4cc59bb7ebf4c290df496f6660f7f
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-17 19:39:42 +0700

    Refactoring1

---
+++ ReservationTests1_9/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReservationTests1_9")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReservationTests1_9")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("81155d8d-5757-47fb-9420-d682fcfad968")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ReservationTests1_9/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReservationTests1_9
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
