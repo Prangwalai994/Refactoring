562073b7ddf9ea367403caf8e43bdc8f97cffcbc
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-17 19:30:19 +0700

    Add project files.

---
+++ RefactoringwithConfidence1_9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoringwithConfidence1_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
